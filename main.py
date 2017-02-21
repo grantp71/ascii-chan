f4c115fef1d0d833d99230b31fe05ba99e769fe8
Grant Patterson <grantrpatterson@gmail.com>
2017-02-21 10:05:12 -0600

    working tuesday a m

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from google.appengine.ext import db
 
 template_dir = os.path.join(os.path.dirname(__file__), 'templates')
 jinja_env = jinja2.Environment(loader = jinja2.FileSystemLoader(template_dir),
-                                autoescape = True)
+                               autoescape = True)
 
 class Handler(webapp2.RequestHandler):
     def write(self, *a, **kw):
@@ -20,23 +20,32 @@ class Handler(webapp2.RequestHandler):
         self.write(self.render_str(template, **kw))
 
 class Art(db.Model):
+    title = db.StringProperty(required = True)
+    art = db.TextProperty(required = True)
+    created = db.DateTimeProperty(auto_now_add = True)
 
 class MainPage(Handler):
     def render_front(self, title="", art="", error=""):
+        arts =  db.GqlQuerry("")
         self.render("front.html", title=title, art=art, error=error)
 
     def get(self):
-        self.render_front()
+        self.render.front()
 
     def post(self):
         title = self.request.get("title")
         art = self.request.get("art")
 
         if title and art:
-            self.write("Thank You")
+            a = Art(title = title, art = art)
+            a.put()
+
+            self.redirect("/")
         else:
             error = "We need both a title and a work of art"
             self.render_front(title, art, error)
+
+
 
 class MainHandler(webapp2.RequestHandler):
     def get(self):
